09dd5bcc2470490c6d71927df081c9564a5f6028
Andrea Venuta <venutawebdesign@gmail.com>
2019-06-22 09:47:32 +0200

    New UI

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { dialog, app, BrowserWindow, globalShortcut, ipcMain } = require('electron')
 const amnesiaclapp = require('amnesiaclapp')
+const path = require('path')
 
 let url
 if (process.env.NODE_ENV === 'dev') {
@@ -15,7 +16,10 @@ const ptrs = {
   DebugRender8: 0x143B65BCC,
   PlayerHide: 0x143B67F5F,
   AllNoUpdateAi: 0x143B67F66,
-  AllNoDamage: 0x143B67F62
+  AllNoDamage: 0x143B67F62,
+  NoGoodsConsume: 0x143b67f59,
+  NoResourceItemConsume: 0x143b67f5a,
+  Quitout: [ 0x143B55048, 0x23C ]
 }
 
 function SekiroProcessComm () {
@@ -24,6 +28,12 @@ function SekiroProcessComm () {
   var position = { x: null, y: null, z: null }
 
   return {
+    checkReattach () {
+      if (!p.attached()) {
+        p.reattach()
+      }
+    },
+
     toggleCollisionMeshes () {
       let w = p.readU32(ptrs.RenderWorld)
       let r0 = p.readU32(ptrs.DebugRender0)
@@ -33,38 +43,62 @@ function SekiroProcessComm () {
         p.writeU32(ptrs.RenderWorld, w & ~1)
         p.writeU32(ptrs.DebugRender0, r0 | 1)
         p.writeU32(ptrs.DebugRender8, r8 | 1)
+        return true
       } else {
         p.writeU32(ptrs.RenderWorld, w | 1)
         p.writeU32(ptrs.DebugRender0, r0 & ~1)
         p.writeU32(ptrs.DebugRender8, r8 & ~1)
+        return false
       }
     },
 
-    toggleHide () {
+    toggleStealth () {
       let h = p.readU32(ptrs.PlayerHide)
       if (h & 1) {
         p.writeU32(ptrs.PlayerHide, h & ~1)
+        return false
       } else {
         p.writeU32(ptrs.PlayerHide, h | 1)
+        return true
       }
     },
 
-    toggleAi () {
+    toggleAI () {
       let h = p.readU32(ptrs.AllNoUpdateAi)
       if (h & 1) {
         p.writeU32(ptrs.AllNoUpdateAi, h & ~1)
+        return false
       } else {
         p.writeU32(ptrs.AllNoUpdateAi, h | 1)
+        return true
       }
     },
 
-    toggleDamage () {
+    toggleNoDamage () {
       let h = p.readU32(ptrs.AllNoDamage)
       if (h & 1) {
         p.writeU32(ptrs.AllNoDamage, h & ~1)
+        return false
       } else {
         p.writeU32(ptrs.AllNoDamage, h | 1)
+        return true
       }
+    },
+
+    toggleConsume () {
+      let h = p.readU32(ptrs.NoGoodsConsume)
+      if (h & 1) {
+        p.writeU32(ptrs.NoGoodsConsume, h & ~1)
+        return false
+      } else {
+        p.writeU32(ptrs.NoGoodsConsume, h | 1)
+        return true
+      }
+    },
+
+    quitout () {
+      let qp = p.pointer(ptrs.Quitout)
+      p.writeU32(qp, 1)
     },
 
     savePosition () {
@@ -96,12 +130,13 @@ let spcomm
 app.on('ready', () => {
    let win = new BrowserWindow({
     width: 300,
-    height: 340,
+    height: 350,
     resizable: (process.env.NODE_ENV == 'dev' ? true : false),
     webPreferences: {
       nodeIntegration: true,
       preload: __dirname + '/preload.js'
-    }
+    },
+    icon: path.join(__dirname, "public/sidekiro.png")
   })
   win.loadURL(url)
   win.setMenuBarVisibility(false)
@@ -121,7 +156,11 @@ app.on('ready', () => {
 
     ipcMain.on('execute', (evt, arg) => {
       if (arg in spcomm) {
-        spcomm[arg]()
+        spcomm.checkReattach()
+        let ret = spcomm[arg]()
+        if (ret === false || ret === true) {
+          evt.reply('flag-change', { flag: arg, value: ret })
+        }
       }
     })
   } catch (err) {
